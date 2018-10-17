65fa06dca40e5813d3b47eda8f0c8bfdd2f6af0f
MinjaeKim <innersme@gmail.com>
2018-10-18 01:52:19 +0900

    Make Movie Constructor

--- src/Movie.java
+++ src/Movie.java
@@ -1,6 +1,13 @@
 public class Movie {
+    static final String CHILDREN = "CHILDREN";
+    static final String NEW_RELEASE = "NEW_RELEASE";
+    static final String REGULAR = "REGULAR";
+
     private String MovieName;
-    private String
-    public Movie() {
+    private String Charge;
+
+    public Movie(String MovieName, String Charge) {
+        this.MovieName = MovieName;
+        this.Charge = Charge;
     }
 }
